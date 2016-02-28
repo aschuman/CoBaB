f300fd326a43dc9818c5ecf1fdacc1895094f15f
Vincent Winkler <vincent.winkler@student.kit.edu>
2016-02-28 16:48:29 +0100

    Created qt project for a 'TestAlgorithm' plugin.

---
+++ TestAlgorithm/TestAlgorithm.h
@@ -0,0 +1,50 @@
+#ifndef _TESTALGORITHM_H
+#define _TESTALGORITHM_H
+
+#include <memory>
+#include "SearchAlgorithm.h"
+#include "SearchQuery.h"
+
+/**
+ * The TestAlgorithm implementation
+ * @author Tung
+ */
+
+class TestAlgorithm: public SearchAlgorithm {
+public:
+
+    TestAlgorithm(const QString& id);
+    
+    QList<DataPacket*> run() override;
+    
+    void cancel() override;
+
+    bool setInputs(const QList<DataPacket*>& inputDataList) override;
+     
+    bool setParameters(const QJsonObject& parameters) override;
+
+    void setName(const QString& name) override;
+
+    void setDescription(const QString& description) override;
+
+    QString getId() const override;
+
+    QString getName() const override;
+
+    QString getDescription() const override;
+    
+    bool supportsProgressInfo() override;
+    
+    void sendIntermediateResults(SearchResult& searchResult) override;
+
+    void sendProgress(double progress, QString message) override;
+
+private: 
+    SearchQuery mQuery;
+    QString mDescription;
+    QList<int> mParameterList;
+    QString mName;
+    QString mId;
+};
+
+#endif //_TESTALGORITHM_H
---
+++ TestAlgorithm/TestAlgorithm.pro
@@ -0,0 +1,16 @@
+QT += core
+#QT -= gui
+
+TARGET = $$qtLibraryTarget(TestAlgorithm)
+
+TEMPLATE = lib
+CONFIG += plugin c++14
+INCLUDEPATH += ../core/interface
+DESTDIR = ../plugins
+
+SOURCES += \
+    TestAlgorithm.cpp
+
+HEADERS += \
+    TestAlgorithm.h
+
